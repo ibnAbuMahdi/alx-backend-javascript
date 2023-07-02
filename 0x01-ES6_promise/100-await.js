f7a0771c39a678aa159591875c0b38aa800e4e36
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-07-02 20:27:32 +0000

    crxn for 100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,10 @@
-import { uploadPhoto, createUser } from './utils'
+import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploadUser(){
-  let photo = uploadPhoto();
-  let user =  createUser();
-  if (await photo && await user){
-    return { photo: photo.then((res)=> res), user: user.then((res)=> res), };
-  } else {
-    return { photo: null, user: null, };
+export default async function asyncUploadUser() {
+  const photo = await uploadPhoto();
+  const user = await createUser();
+  if (photo && user) {
+    return { photo: photo.body, user };
   }
+  return { photo: null, user: null };
 }
