fe8d18fde3ea46212a128815d4dd1352acd2cd27
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-07-04 21:32:41 +0000

    crxn 2 for 8 and 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,26 @@
 export const weakMap = new WeakMap();
+
 export function queryAPI(endpoint) {
-  const val = weakMap.get(endpoint);
+  let val = weakMap.get(endpoint);
+
+  if (typeof val !== 'number') {
+    val = 0;
+  }
+
   if (val >= 5) {
     throw new Error('Endpoint load is high');
   } else if (val > 0) {
     weakMap.set(endpoint, val + 1);
-  } else if ('protocol' in endpoint && 'name' in endpoint) {
+  } else if (
+    typeof endpoint === 'object'
+    && endpoint !== null
+    && 'protocol' in endpoint
+    && typeof endpoint.protocol === 'string'
+    && 'name' in endpoint
+    && typeof endpoint.name === 'string'
+  ) {
     weakMap.set(endpoint, 1);
+  } else {
+    throw new Error('Invalid endpoint');
   }
 }
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,26 @@
 export default function cleanSet(set, start) {
+  if (!(set instanceof Set)) {
+    throw new Error('Invalid input: set must be an instance of Set');
+  }
+
+  if (typeof start !== 'string') {
+    throw new Error('Invalid input: start must be a string');
+  }
+
   let out = '';
+
   for (const str of set) {
-    if (set instanceof Set && start.length && str.startsWith(start)) {
-      if (out.length) {
+    if (typeof str !== 'string') {
+      throw new Error('Invalid input: set elements must be strings');
+    }
+
+    if (start.length > 0 && str.startsWith(start)) {
+      if (out.length > 0) {
         out += '-';
       }
       out += str.substring(start.length);
     }
   }
+
   return out;
 }
