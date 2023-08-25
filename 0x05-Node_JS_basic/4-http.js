dc6e785873305d1d0ae0b7f694eba29b9dc13e22
Your Name <you@example.com>
2023-08-25 21:49:42 +0000

    crxn 1 for 5

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,10 +4,8 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-//  res.writeHead(100, {'Content-Type': 'text/plain'});
-  res.setHeader("content-Type", "text/plain");
+  res.setHeader('content-Type', 'text/plain');
   res.end('Hello Holberton School!');
-//  res.end();
 });
 
 app.listen(port, hostname);
