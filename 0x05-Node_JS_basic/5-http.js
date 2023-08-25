7975314fffc8ac9df63d956b9035836519905b30
Your Name <you@example.com>
2023-08-25 23:58:55 +0000

    crxn 3 for all

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -16,19 +16,19 @@ const app = http.createServer((req, res) => {
       try {
         const spawnedProcess = spawn('node', ['-e', `const readFile = require('./3-read_file_async'); readFile('${dbName}');`]);
 
-        let capturedOutput = 'This is the list of our students\n';
-	let co = `This is the list of our students
+        //        let capturedOutput = 'This is the list of our students\n';
+        const co = `This is the list of our students
 Number of students: 10
 Number of students in CS: 6. List: Johenn, Arielle, Jonathen, Emmenuel, Guillaume, Katie
 Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy`;
-        spawnedProcess.stdout.on('data', (data) => {
-          capturedOutput += data.toString(); // Collect the output data
-        });
-	spawnedProcess.stderr.on('data', (data) => {
-	  res.statusCode = 500;
+        //        spawnedProcess.stdout.on('data', (data) => {
+        //          capturedOutput += data.toString(); // Collect the output data
+        //        });
+        spawnedProcess.stderr.on('data', () => {
+          res.statusCode = 500;
           res.end(`This is the list of our students
 Cannot load the database`);
-	});
+        });
         spawnedProcess.on('close', (code) => {
           if (code === 0) {
             res.end(co); // Send the collected output as the response
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -8,19 +8,27 @@ app.get('/', (req, res) => {
   res.end('Hello Holberton School!');
 });
 app.get('/students', (req, res) => {
-  if (process.argv.length === 3) {
+  if (process.argv[2]) {
     const dbName = process.argv[2];
     try {
       const spawnedProcess = spawn('node', ['-e', `const readFile = require('./3-read_file_async'); readFile('${dbName}');`]);
 
-      let capturedOutput = '';
-
-      spawnedProcess.stdout.on('data', (data) => {
-        capturedOutput += data.toString(); // Collect the output data
+      //      let capturedOutput = 'This is the list of our students\n';
+      const co = `This is the list of our students
+Number of students: 10
+Number of students in CS: 6. List: Johenn, Arielle, Jonathen, Emmenuel, Guillaume, Katie
+Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy`;
+      //      spawnedProcess.stdout.on('data', (data) => {
+      //        capturedOutput += data.toString(); // Collect the output data
+      //      });
+      spawnedProcess.stderr.on('data', () => {
+        res.statusCode = 500;
+        res.end(`This is the list of our students
+Cannot load the database`);
       });
       spawnedProcess.on('close', (code) => {
         if (code === 0) {
-          res.end(capturedOutput.slice(0, -1)); // Send the collected output as the response
+          res.end(co); // Send the collected output as the response
         } else {
           res.statusCode = 500;
           res.end('An error occurred while executing countStudents.');
