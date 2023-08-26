0b19c038f3b80cd7116ae324233faabadf6b0270
Your Name <you@example.com>
2023-08-26 02:57:24 +0000

    crxn 5 for 6

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,11 +1,10 @@
 function getPaymentTokenFromAPI(success) {
-  if (success) {
-    return new Promise((resolve, reject) => {
+  if (success === true) {
+    return new Promise((resolve) => {
             resolve({ data: 'Successful response from the API' });
     });
-  } else {
-  	return
   }
+  return
 }
 
 module.exports = getPaymentTokenFromAPI
